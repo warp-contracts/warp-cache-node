7180b257979b0a9c46beb48a12f2999e4729ffad
Asia <ziola.jm@gmail.com>
2024-08-20 11:44:36 +0200

    onNewState - logging

--- src/routes/agg/onState.js
+++ src/routes/agg/onState.js
@@ -11,11 +11,17 @@ module.exports = {
   onNewState: async function (data) {
     const { contractTxId, result } = data;
     const contractState = result.cachedValue.state;
+    console.time('balancesLastSortKey');
     const lastSK = await balancesLastSortKey(contractTxId);
+    console.timeEnd('balancesLastSortKey');
 
     if (result.sortKey.localeCompare(lastSK)) {
+      console.time('changeWalletAddressIfNeeded');
       await changeWalletAddressIfNeeded(data);
+      console.timeEnd('changeWalletAddressIfNeeded');
+      console.time('upsertBalances');
       await upsertBalances(contractTxId, result.sortKey, contractState);
+      console.timeEnd('upsertBalances');
     } else {
       logger.warn('Received state with older or equal sort key', {
         contract: contractTxId,
@@ -27,16 +33,23 @@ module.exports = {
 };
 
 async function changeWalletAddressIfNeeded(data) {
+  console.time('getParsedInput');
   const input = getParsedInput(data);
+  console.timeEnd('getParsedInput');
   if (!input) {
     return;
   } else if (shouldChangeWalletAddress(input)) {
     const { id, address } = getUserInfo(input);
     logger.debug(`New 'changeWallet' interaction, update required. Id: ${id}, new address: ${address}.`);
     if (id && address) {
+      console.time('getWarpyLastUserAddress');
       const oldAddress = (await getWarpyLastUserAddress(id))[0]?.value;
+      console.timeEnd('getWarpyLastUserAddress');
+
       if (oldAddress) {
+        console.time('updateWalletAddress');
         await updateWalletAddress(oldAddress, address);
+        console.timeEnd('updateWalletAddress');
         logger.info(`Wallet address updated. Old address: ${oldAddress}, new address: ${address}`);
       } else {
         logger.debug(`No data found in balances for user: ${id}.`);
